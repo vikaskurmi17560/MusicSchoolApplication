0fa0c3fa55bf4d975e5659a6fa93143e154ed992
vikas kurmi <154617102+vikaskurmi17560@users.noreply.github.com>
2025-01-24 07:44:45 +0000

    clear backend

--- Backend/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const env = require('dotenv');//import .env file
-const Mongoose = require("mongoose");//import mongoose
-env.config();
-Mongoose.connect(process.env.MONGO_URI).then(() => { console.log("Connect Database") }).catch(err => console.log(err))
-app.listen(process.env.PORT, () => {
-    console.log(`Server run at port ${process.env.PORT}`);
-})
