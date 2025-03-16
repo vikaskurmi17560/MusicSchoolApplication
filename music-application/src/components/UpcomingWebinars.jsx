bda81e65449784b299ab12293ca910dc060aef5c
vikas kurmi <154617102+vikaskurmi17560@users.noreply.github.com>
2025-03-16 15:07:51 +0000

    .

--- music-application/src/components/UpcomingWebinars.jsx
+++ music-application/src/components/UpcomingWebinars.jsx
@@ -94,7 +94,7 @@ function UpcomingWebinars() {
                 </div>
 
                 <div className="mt-10 text-center">
-                    <Link href="/webinars"
+                    <Link href="#"
                         className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
                     >
                         View All webinars
